d69fae702805aba9eabfef07072c1852afb1d158
Sinem Uyarer <sinemuyarer@gmail.com>
2021-01-19 09:07:20 +0100

    update

--- project-01/script.js
+++ project-01/script.js
@@ -1,13 +1,11 @@
 (function () {
     var hamburger = document.getElementById("menu-button");
     var overlay = document.getElementsByClassName("overlay");
-    var sidenav = document.getElementsByClassName("side-nav");
     var p = document.getElementById("move");
     var quit = document.getElementById("topx");
 
     hamburger.addEventListener("click", function () {
-        overlay.classList.remove("on");
-        sidenav.classList.remove("on");
+        overlay.classList.add("on");
     });
 
     p.addEventListener("click", function () {
