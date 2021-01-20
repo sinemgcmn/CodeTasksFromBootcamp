4f16c12bf4ec499ae2d6b063878eb64760a753b1
Sinem Uyarer <sinemuyarer@gmail.com>
2021-01-20 18:31:17 +0100

    going to the img but not checking transtion and highlight yet

---
+++ project-kitty/script_s.js
@@ -0,0 +1,47 @@
+(function () {
+    var kitties = document.querySelectorAll("#kitties img");
+    var dots = document.querySelectorAll(".dots div");
+    var currentKitty = 0;
+    var total = kitties.length;
+    var timer; // will correct this
+
+    /* FIRST PART */
+
+    function moveKitties() {
+        kitties[currentKitty].classList.remove("onscreen");
+        kitties[currentKitty].classList.add("exit");
+        dots[currentKitty].classList.remove("on"); //white is moving
+        currentKitty++;
+        if (currentKitty >= total) {
+            currentKitty = 0;
+        }
+        kitties[currentKitty].classList.add("onscreen"); // kitty continue
+        dots[currentKitty].classList.add("on"); //kitty continue
+        console.log(currentKitty);
+        setTimeout(moveKitties, 5000);
+    }
+    setTimeout(moveKitties, 1000);
+
+    document.addEventListener("transitionend", function (event) {
+        if (event.target.classList == "exit") {
+            event.target.classList.remove("exit");
+        }
+    });
+
+    /* SECOND PART -CLICK*/
+
+    /* With the below one I know which one is clicked anymore */
+
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("click", function (e) {
+            for (var i = 0; i < dots.length; i++) {
+                if (dots[i] == e.target) {
+                    console.log(i);
+                    currentKitty = dots[i];
+                    break;
+                }
+            }
+            clearTimeout(timer);
+        });
+    }
+})();
