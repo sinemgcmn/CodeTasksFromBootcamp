aeea1ebe8cb58de98a9ee635a09ef86554df0991
Sinem Uyarer <sinemuyarer@gmail.com>
2021-01-20 16:28:44 +0100

    user click determine added

--- project-kitty/script.js
+++ project-kitty/script.js
@@ -4,6 +4,12 @@
     var currentKitty = 0;
     var total = kitties.length;
 
+    /* FIRST PART */
+
+    var timer = setTimeout(function () {
+        console.log("Hello");
+    }, 3000);
+
     function moveKitties() {
         kitties[currentKitty].classList.remove("onscreen");
         kitties[currentKitty].classList.add("exit");
@@ -17,7 +23,23 @@
         console.log(currentKitty);
         setTimeout(moveKitties, 5000);
     }
-    setTimeout(moveKitties, 1000);
+    setTimeout(moveKitties, 5000);
+
+    /* SECOND PART -CLICK*/
+
+    /* With the below one I know which one is clicked anymore */
+
+    for (var i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("click", function (e) {
+            for (var i = 0; i < dots.length; i++) {
+                if (dots[i] == e.target) {
+                    console.log(i);
+                    break;
+                }
+            }
+            clearTimeout(timer);
+        });
+    }
 
     document.addEventListener("transitionend", function (event) {
         if (event.target.classList == "exit") {
