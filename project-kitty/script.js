3021585c8830efc6d8b131b508fe32e79fd55fa6
Sinem Uyarer <sinemuyarer@gmail.com>
2021-01-20 15:21:56 +0100

    part-l

--- project-kitty/script.js
+++ project-kitty/script.js
@@ -5,14 +5,11 @@
     console.log("kitties: ", kitties);
 
     function moveKitties() {
-        kitties[currentKitty].classList.add("onscreen");
         kitties[currentKitty].classList.remove("onscreen");
         kitties[currentKitty].classList.add("exit");
         currentKitty++;
-        if (currentKitty == total - 1) {
+        if (currentKitty >= total) {
             currentKitty = 0;
-        } else {
-            currentKitty;
         }
         kitties[currentKitty].classList.add("onscreen");
         console.log(currentKitty);
@@ -20,11 +17,9 @@
     }
     setTimeout(moveKitties, 1000);
 
-    // document.addEventListener("transitionend", function (e) {
-    //     if (kitties[currentKitty].classList.contains("exit")) {
-    //         kitties[currentKitty].classList.remove("exit");
-    //     } else {
-    //         console.log("transition ended....", e.target.classList);
-    //     }
-    // });
+    document.addEventListener("transitionend", function (event) {
+        if (event.target.classList == "exit") {
+            event.target.classList.remove("exit");
+        }
+    });
 })();
