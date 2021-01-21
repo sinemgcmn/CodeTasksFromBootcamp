cc9d4889b16966020c9538af9f1da617ea8cb5d1
Sinem Uyarer <sinemuyarer@gmail.com>
2021-01-21 21:35:55 +0100

    project-pane added

---
+++ project-pane/script.js
@@ -0,0 +1,28 @@
+var containerJQ = $(".container");
+var sliderJQ = $(".slider");
+var topJQ = $(".top");
+
+var initialXPositionOfSlider = sliderJQ.offset().left;
+var distance = 0;
+var isMoving = false;
+
+sliderJQ.mousedown(function (e) {
+    isMoving = true;
+    containerJQ.mousemove(function (e) {
+        if (isMoving) {
+            var x = e.clientX;
+            if (x > containerJQ.width()) return false;
+            distance = initialXPositionOfSlider - x;
+            topJQ.css("width", initialXPositionOfSlider - distance);
+            sliderJQ.css("left", x - sliderJQ.width() / 2);
+        }
+    });
+});
+
+sliderJQ.mouseup(function (e) {
+    isMoving = false;
+});
+
+sliderJQ.mouseleave(function () {
+    console.log("A mouseleave was captured on the document!");
+});
