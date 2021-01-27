7aa7db4d93a71a3cafa2b6037d0a70cbb014231a
Sinem Uyarer <sinemuyarer@gmail.com>
2021-01-27 16:56:42 +0100

    script changed

--- project-ticker/script.js
+++ project-ticker/script.js
@@ -1,45 +1,33 @@
 (function () {
-    ticker("ticker1", -1);
+    var ticker = $("#ticker");
+    var headlines = ticker.find(".headlines");
+    var links = headlines.find("a");
+    var curX = headlines.offset().left;
+    var linkWidth = links.eq(0).outerWidth();
+    var animId;
 
-    ticker("ticker2", 1);
+    headlines.on("mouseenter", function (e) {
+        cancelAnimationFrame(animId);
+    });
 
-    function ticker(id, step) {
-        var ticker = document.getElementById(id);
-        var headlines = ticker.querySelector(".headlines");
-        var links = headlines.getElementsByTagName("a");
-        var left = headlines.offsetLeft;
-        var headlinesWidth = headlines.offsetWidth;
-        var tickerWidth = ticker.offsetWidth;
-        var linkWidth =
-            step < 0
-                ? links[0].offsetWidth
-                : links[links.length - 1].offsetWidth;
-        var animId;
+    headlines.on("mouseleave", function () {
+        moveHeadlines();
+    });
 
-        headlines.addEventListener("mouseenter", function () {
-            cancelAnimationFrame(animId);
-        });
+    moveHeadlines();
 
-        headlines.addEventListener("mouseleave", function () {
-            moveHeadLines();
-        });
-
-        moveHeadLines();
-
-        function moveHeadLines() {
-            left += step;
-            if (step < 0 && left < -linkWidth) {
-                left += linkWidth;
-                headlines.appendChild(links[0]);
-                linkWidth = links[0].offsetWidth;
-            }
-            if (step > 0 && left + headlinesWidth > tickerWidth + linkWidth) {
-                left -= linkWidth;
-                headlines.insertBefore(links[links.length - 1], links[0]);
-                linkWidth = links[links.length - 1].offsetWidth;
-            }
-            headlines.style.left = left + "px";
-            animId = requestAnimationFrame(moveHeadLines);
+    function moveHeadlines() {
+        curX--;
+        console.log(curX, -linkWidth);
+        if (curX < -linkWidth) {
+            curX += linkWidth;
+            links.eq(0).appendTo(headlines);
+            links = headlines.find("a");
+            linkWidth = links.eq(0).outerWidth();
         }
+        headlines.css({
+            left: curX + "px",
+        });
+        animId = requestAnimationFrame(moveHeadlines);
     }
 })();
