bf99f307036fa01d828013da65a67f14946da9d1
Sinem Uyarer <sinemuyarer@gmail.com>
2021-01-28 21:35:34 +0100

    all working

--- project-spotify/script.js
+++ project-spotify/script.js
@@ -1,10 +1,12 @@
 (function () {
+    var more = $(".more");
+    more.hide();
+    var nextUrl;
+
     $(".submit-button").on("click", function () {
-        // console.log("button was click");
         var userInput = $("input").val();
         var artistOrAlbum = $("select").val();
-        console.log("user data:", userInput, "-", artistOrAlbum);
-
+        // console.log("user data:", userInput, "-", artistOrAlbum);
         $.ajax({
             method: "GET",
             url: "https://spicedify.herokuapp.com/spotify",
@@ -13,36 +15,72 @@
                 type: artistOrAlbum,
             },
             success: function (response) {
-                response = response.artists || response.albums;
-                console.log("response:", response);
-
-                var resultsHtml = "";
-                for (var i = 0; i < response.items.length; i++) {
-                    var defaultImage =
-                        "https://bitsofco.de/content/images/2018/12/broken-1.png";
-                    if (response.items[i].images.length > 0) {
-                        defaultImage = response.items[i].images[0].url;
-                    }
-                    resultsHtml +=
-                        "<div>" +
-                        response.items[i].name +
-                        "</div>" +
-                        '<img src="' +
-                        defaultImage +
-                        '"/>';
-                    // console.log("resultsHtml:", resultsHtml);
-                    $(".results-container").html(resultsHtml);
-
-                    console.log("response.next:", response.next);
-                    var nextUrl =
-                        response.next &&
-                        response.next.replace(
-                            "api.spotify.com/v1/search",
-                            "spicedify.herokuapp.com/spotify"
-                        );
-                    console.log(" nextUrl:", nextUrl);
-                }
+                callSpotify(response, userInput);
             },
         });
     });
+
+    $(".more").on("click", function () {
+        var userInput = $("input").val();
+        var artistOrAlbum = $("select").val();
+        $.ajax({
+            method: "GET",
+            url: nextUrl,
+            data: {
+                query: userInput,
+                type: artistOrAlbum,
+            },
+            success: function (response) {
+                callSpotify(response, userInput);
+            },
+        });
+    });
+
+    function callSpotify(response, userInput) {
+        response = response.artists || response.albums;
+        console.log("response:", response);
+        var resultsHtml = "";
+        resultsHtml = "<p>" + 'Results for "' + userInput + '" 🎃 ' + "</p>";
+
+        if (response.items.length == 0) {
+            resultsHtml += "<p>Nothing to see here...</p>";
+        } else {
+            for (var i = 0; i < response.items.length; i++) {
+                var defaultImage =
+                    "https://bitsofco.de/content/images/2018/12/broken-1.png";
+
+                if (response.items[i].images.length > 0) {
+                    defaultImage = response.items[i].images[0].url;
+                }
+                var exUrl = response.items[i].external_urls.spotify;
+
+                resultsHtml +=
+                    "<a href=" +
+                    exUrl +
+                    '">' +
+                    "<div>" +
+                    response.items[i].name +
+                    "</div>" +
+                    '<img src="' +
+                    defaultImage +
+                    '"/> </a>';
+            }
+        }
+        $(".results-container").html(resultsHtml);
+        // console.log("response.next:", response.next);
+
+        ///More button check
+
+        if (response.next == null) {
+            more.css("display", "none");
+        } else {
+            more.toggle();
+            nextUrl =
+                response.next &&
+                response.next.replace(
+                    "api.spotify.com/v1/search",
+                    "spicedify.herokuapp.com/spotify"
+                );
+        }
+    }
 })();
