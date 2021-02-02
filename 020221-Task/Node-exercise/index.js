c1a0c204b93061a59c3a7d9d255f65c2677b111f
Sinem Uyarer <sinemuyarer@gmail.com>
2021-02-02 21:12:49 +0100

    morning exercise is done-working

---
+++ 020221-Task/Node-exercise/index.js
@@ -0,0 +1,32 @@
+var myUrl = process.argv[2];
+// console.log(myUrl);
+
+function getAndParse(newUrl) {
+    const url = require("url");
+    const myObj = url.parse(newUrl);
+    console.log(
+        `
+    The protocol is ${myObj.protocol}
+    The host is  ${myObj.host}
+    The hostname is ${myObj.hostname}
+    The port is ${myObj.port}
+    The pathname is ${myObj.pathname}
+    The query is ${myObj.query}
+    `
+    );
+    getStringParse(myObj.query);
+}
+
+getAndParse(myUrl);
+
+function getStringParse(test) {
+    const querystring = require("querystring");
+    const myStr = querystring.parse(test);
+    for (const property in myStr) {
+        console.log(
+            `The value of the ${property} parameter is ${myStr[property]}`
+        );
+    }
+}
+
+getStringParse();
