dcd135870e7b4784c8d7c98e9c664b7a906d22c1
Sinem Uyarer <sinemuyarer@gmail.com>
2021-02-03 21:05:54 +0100

    Part-2 is ready, working

---
+++ 030221-Task/Part-2/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+function mapSizes(pathToDirectory) {
+    const fileObj = fs.readdirSync(pathToDirectory, { withFileTypes: true });
+    const newObj = {};
+    fileObj.forEach((file) => {
+        // console.log(file);
+        if (file.isFile()) {
+            // console.log(file);
+            const stats = fs.statSync(pathToDirectory + "/" + file.name);
+            const sizeNum = stats.size;
+            newObj[file.name] = sizeNum;
+        } else if (file.isDirectory) {
+            newObj[file.name] = mapSizes(pathToDirectory + "/" + file.name);
+        }
+    });
+    return newObj;
+}
+
+// mapSizes("/Users/sinemgocmenuyarer/desktop/Part-2");
+// fs.writeFileSync("files.json", JSON.stringify(mapSizes, null, 4));
+const sizeMap = mapSizes("/Users/aceeegimmnnorrsuy / desktop / Part - 2");
+fs.writeFileSync("files.json", JSON.stringify(sizeMap, null, 4));
